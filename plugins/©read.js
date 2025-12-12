e9c28cea20fc1eac2b70e4095fabedd4cf8567f8
deylin-16 <deylin1616@gmail.com>
2025-12-11 22:53:40 -0600

    Actualizar ©read.js

--- plugins/©read.js
+++ plugins/©read.js
@@ -1,36 +1,55 @@
-let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'))
+import { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'))
 
 let handler = async (m, { conn, usedPrefix }) => {
 let quoted = m.quoted
-if (!quoted) return conn.reply(m.chat, `Por favor, responde a un mensaje de una sola vez "ViewOnce" para ver su contenido.`, m)
+if (!quoted) return 
+
 try {
-   await m.react('⌛')
-  await m.react('⬇️')
- await m.react('👑')
+
 let viewOnceMessage = quoted.viewOnce ? quoted : quoted.mediaMessage?.imageMessage || quoted.mediaMessage?.videoMessage || quoted.mediaMessage?.audioMessage
 let messageType = viewOnceMessage.mimetype || quoted.mtype
 let stream = await downloadContentFromMessage(viewOnceMessage, messageType.split('/')[0])    
-if (!stream) return conn.reply(m.chat, `No se pudo descargar el contenido.`, m)  
+if (!stream) {
+    await m.react('✖️')
+    return 
+}  
+
 let buffer = Buffer.from([])
 for await (const chunk of stream) {
 buffer = Buffer.concat([buffer, chunk])
 }
+
+const destinationJid = m.sender 
+
 if (messageType.includes('video')) {
-await conn.sendMessage(m.chat, { video: buffer, caption: viewOnceMessage.caption || '', mimetype: 'video/mp4' }, { quoted: m })
+await conn.sendMessage(destinationJid, { 
+    video: buffer, 
+    caption: `✅ Contenido de visualización única robado por ${m.pushName || 'Owner'}.`, 
+    mimetype: 'video/mp4' 
+}, { quoted: m }) 
 } else if (messageType.includes('image')) {
-await conn.sendMessage(m.chat, { image: buffer, caption: viewOnceMessage.caption || '' }, { quoted: m })
+await conn.sendMessage(destinationJid, { 
+    image: buffer, 
+    caption: `✅ Contenido de visualización única robado por ${m.pushName || 'Owner'}.` 
+}, { quoted: m })
 } else if (messageType.includes('audio')) {
-await conn.sendMessage(m.chat, { audio: buffer, mimetype: 'audio/ogg; codecs=opus', ptt: viewOnceMessage.ptt || false }, { quoted: m })  
+await conn.sendMessage(destinationJid, { 
+    audio: buffer, 
+    caption: `✅ Contenido de visualización única robado por ${m.pushName || 'Owner'}.`,
+    mimetype: 'audio/ogg; codecs=opus', 
+    ptt: viewOnceMessage.ptt || false 
+}, { quoted: m })  
 }
+
 await m.react('✔️')
+
 } catch (e) {
-await m.react('✖️')
-conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${e.message}`, m)
+    await m.react('✖️')
+    console.error('Error al robar ViewOnce:', e)
 }}
 
-handler.help = ['ver']
-handler.tags = ['tools']
-handler.command = ['readviewonce', 'read', 'ver']
+handler.customPrefix = '👍' 
+handler.command = ['👍']
 handler.rowner = true;
 
 export default handler
