23ee9a43d8af7afdbffdec11b900db182deda357
deylin-16 <deylin1616@gmail.com>
2025-12-25 00:19:50 -0600

    Actualizar ©prueba.js

--- plugins/©prueba.js
+++ plugins/©prueba.js
@@ -1,7 +1,6 @@
 let handler = async (m, { conn }) => {
     const config = global.getAssistantConfig(conn.user.jid)
     let targetUrl = 'https://www.deylin.xyz'
-    // Usamos el enlace de IBB que sí carga
     let fixedImage = 'https://i.ibb.co/g8PsK57/IMG-20251224-WA0617.jpg'
 
     await conn.sendMessage(m.chat, {
@@ -10,13 +9,11 @@ let handler = async (m, { conn }) => {
             externalAdReply: {
                 title: `CÓDIGO DE EMPAREJAMIENTO`,
                 body: `Asistente: ${config.assistantName}`,
-                mediaType: 1, // Tipo 1 es "Link", no intenta abrir visor de fotos
-                renderLargerThumbnail: true,
+                mediaType: 1, // Cambiado a 1 para que sea un link normal
+                renderLargerThumbnail: false, // Imagen pequeña a la derecha
                 thumbnailUrl: fixedImage,
                 sourceUrl: targetUrl,
-                // IMPORTANTE: Dejar mediaUrl vacío o eliminarlo
-                mediaUrl: null, 
-                showAdAttribution: true
+                showAdAttribution: false // Esto elimina el texto de "anuncio"
             }
         }
     }, { quoted: m })
