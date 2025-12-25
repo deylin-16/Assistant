42de3cf5722cd405e551cdb1d49c88600f9c7dca
deylin-16 <deylin1616@gmail.com>
2025-12-24 22:42:03 -0600

    Actualizar ©prueba.js

--- plugins/©prueba.js
+++ plugins/©prueba.js
@@ -1,27 +1,36 @@
-let handler = async (m, { conn }) => {
-    const config = global.getAssistantConfig(conn.user.jid)
-    
-    let isBuffer = Buffer.isBuffer(config.assistantImage)
-    let targetUrl = 'https://www.deylin.xyz'
+import ws from 'ws';
 
-    await conn.sendMessage(m.chat, {
-        text: targetUrl,
-        contextInfo: {
-            externalAdReply: {
-                title: `CÓDIGO DE EMPAREJAMIENTO`,
-                body: `Asistente: ${config.assistantName}`,
-                mediaType: 3,
-                renderLargerThumbnail: true,
-                thumbnail: isBuffer ? config.assistantImage : null,
-                thumbnailUrl: !isBuffer ? config.assistantImage : null,
-                sourceUrl: targetUrl,
-                showAdAttribution: true,
-                containsAutoReply: true
-            }
-        }
-    }, { quoted: m })
+let handler = async (m, { conn: _envio, usedPrefix }) => {
+  const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
+
+  function convertirMs(ms) {
+    var segundos = Math.floor(ms / 1000);
+    var minutos = Math.floor(segundos / 60);
+    var horas = Math.floor(minutos / 60);
+    var días = Math.floor(horas / 24);
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    var resultado = "";
+    if (días !== 0) resultado += días + "d ";
+    if (horas !== 0) resultado += horas + "h ";
+    if (minutos !== 0) resultado += minutos + "m ";
+    if (segundos !== 0) resultado += segundos + "s";
+    return resultado.trim();
+  }
+
+  const message = users.map((v, i) => 
+`[ Sub-Bot #${i + 1} ]
+• Nombre: ${v.user.name || 'Asistente'}
+• Enlace: wa.me/${v.user.jid.replace(/[^0-9]/g, '')}
+• Tiempo: ${v.uptime ? convertirMs(Date.now() - v.uptime) : '---'}`).join('\n\n');
+
+  const responseMessage = `*LISTA DE VINCULACIÓNES*\n\nConectados: ${users.length}\n\n${message || 'No hay sub-bots activos.'}`.trim();
+
+  await _envio.sendMessage(m.chat, { text: responseMessage, mentions: _envio.parseMention(responseMessage) }, { quoted: m })
 }
 
-handler.command = ['prueba']
+
+handler.command = ['vinculaciones]
 
 export default handler
